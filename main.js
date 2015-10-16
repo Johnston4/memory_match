bfc3d16c6c7ad644cb31f7c24dac7cb37b769a04
Sara Johnston <sarajohnston4@gmail.com>
2015-10-16 14:45:03 -0700

    memory match v0.5 in progress - Sara Johnston

--- main.js
+++ main.js
@@ -1,8 +1,42 @@
 var first_card_clicked = null;
 var second_card_clicked = null;
 var total_possible_matches = 2;
+var match_counter = 0;
 
 function card_clicked(card_element){
-    var back_face = $(card_element).find('.back');
-    $(back_face).toggle();
+    console.log("card_element is " + card_element);
+
+    var card_image_back= $(card_element).find('img');
+    $(card_image_back).hide();
+
+    var front_card = $(card_element).prev().find('img');
+    var first_front_card_image = $(front_card).attr('src');
+        console.log('first_front_card_image IS ' + first_front_card_image);
+    if (first_card_clicked == null) {
+        console.log('this is the first card we clicked');
+        first_card_clicked = first_front_card_image;
+        console.log('first card clicked is ' + first_card_clicked)
+    }
+    else {
+        console.log('this is the second card we clicked');
+        var second_card = $(card_element).prev().find('img');
+        var second_front_card_image = $(second_card).attr('src');
+        console.log('second_front_card_image is ' + second_front_card_image);
+        second_card_clicked = second_front_card_image;
+        console.log('second_card_clicked equals ' + second_card_clicked);
+        console.log('first card clicked equals ' + first_card_clicked);
+        if (first_card_clicked == second_card_clicked) {
+            console.log('Is it a match? Yes it is ' + (first_card_clicked == second_card_clicked));
+            match_counter += 1;
+            console.log('match_counter value is ' + match_counter);
+            first_card_clicked = null;
+            second_card_clicked = null;
+            console.log('first_card_clicked, second_card_clicked is now... ' + first_card_clicked + ', ' + second_card_clicked + ' AGAIN. You can pick another card.')
+        }
+        else {
+            console.log('Is it a match? No it is ' + (first_card_clicked == second_card_clicked));
+            setTimeout(function(){ $(card_image_back).show(); }, 3000);
+
+        }
+    }
 }
